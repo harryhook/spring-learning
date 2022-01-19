f75a26c8c6bf140286240fd3a68ee8ef201ca796
chenhaowei002 <chenhaowei002.com>
2022-01-19 22:31:39 +0800

    redis sentinel

--- spring-data-demo/src/test/java/com/coderunning/SpringJedisDemo.java
+++ spring-data-demo/src/test/java/com/coderunning/SpringJedisDemo.java
@@ -1,13 +1,15 @@
 package com.coderunning;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
+@Slf4j
 public class SpringJedisDemo {
 
     @Test
@@ -85,6 +87,70 @@ public class SpringJedisDemo {
             pipeline.syncAndReturnAll();
         }
         System.out.println(System.currentTimeMillis());
+
+
+    }
+
+
+    @Test
+    public void testJedisSentinel() {
+
+        Set<String> sentinels = new HashSet<String>();
+        sentinels.add("127.0.0.1:26379");
+        sentinels.add("127.0.0.1:26380");
+        sentinels.add("127.0.0.1:26381");
+        
+        JedisSentinelPool sentinelPool = new JedisSentinelPool("mymaster", sentinels);
+
+        try (Jedis jedis = sentinelPool.getResource()) {
+            System.out.println(System.currentTimeMillis());
+            for (int i = 0; i < 100000; i++) {
+
+                jedis.hset("hashKey:" + i, "field:" + i, "value:" + i);
+
+            }
+
+
+        }
+        System.out.println(System.currentTimeMillis());
+
+
+    }
+
+    @Test
+    public void testJedisSentinelFailover() {
+
+        Set<String> sentinels = new HashSet<>();
+        sentinels.add("127.0.0.1:26379");
+        sentinels.add("127.0.0.1:26380");
+        sentinels.add("127.0.0.1:26381");
+
+        JedisSentinelPool sentinelPool = new JedisSentinelPool("mymaster", sentinels);
+
+        int count = 0;
+        while(true) {
+            Jedis jedis = null;
+
+            try {
+                jedis = sentinelPool.getResource();
+                jedis.hset("hashKey:" + count, "field:" + count, "value:" + count);
+
+
+                if(count % 200 == 0) {
+                    Thread.sleep(100);
+                    log.info(jedis.hget("hashKey:" + count, "field:" + count));
+                }
+                count++;
+            }catch (Exception e) {
+                e.printStackTrace();
+            }finally {
+
+            if(jedis != null ){
+                jedis.close();
+                }
+            }
+        }
+
 
 
     }
