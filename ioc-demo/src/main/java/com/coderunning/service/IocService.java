c43f08a6247757245a6db26eeadf20cf0e27d968
harryhook <haowei.chen@jiduauto.com>
2023-01-01 14:48:06 +0800

    circle dependency

--- ioc-demo/src/main/java/com/coderunning/service/IocService.java
+++ ioc-demo/src/main/java/com/coderunning/service/IocService.java
@@ -1,7 +1,19 @@
 package com.coderunning.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
+@Slf4j
 public class IocService {
+
+    @Resource
+    private ColorService colorService;
+
+    public ColorService getColorService() {
+        return colorService;
+    }
 }
